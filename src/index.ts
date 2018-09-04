40239bfa8aa66c964eacfb3cfa381bab1f57c428
Eyk <eykrehbein@gmail.com>
2018-09-04 20:52:27 +0200

    :children_crossing: Add handler to open help if invalid or no command given

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 import { initBranchCommands } from './branch/branchCommands';
 import { initGeneralCommands } from './general/generalCommands';
-initBranchCommands();
-initGeneralCommands();
+import * as program from 'commander';
+
+const init = async () => {
+  await initBranchCommands();
+  await initGeneralCommands();
+} 
+
+init().then(() => {
+  program.on('command:*', function () {
+    console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '));
+    program.help()
+  });
+  if(process.argv.length === 2){
+    program.help();
+  }
+})
+
